531ef3291e11eac530933cde3f68dbd172731eb0
ffernandezmsysman <nfmaje@sysman.com.co>
2025-07-17 23:38:01 -0500

    Eliminando contenido anterior

--- app/ai/config/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Cargar variables de entorno
-load_dotenv()
-
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-print(GOOGLE_API_KEY)
-#OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") Cuando se tenga GPT
--- app/ai/llm_models/llm_base.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC, abstractmethod
-
-class BaseLLM:
-    async def generate_response(self, context, query):
-        raise NotImplementedError
